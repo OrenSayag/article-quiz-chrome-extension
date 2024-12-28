bc0347015151c3a6150908d497e18917aaf80cb2
Oren Sayag <orensayag500@gmail.com>
2024-12-28 15:41:43 +0200

    aq-71 - ✨ dev quiz button - just display popup

---
+++ components/quiz/quiz-dialog/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import { cn } from "@/lib/utils.ts";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { useTheme } from "@/components/theme-provider.tsx";
+
+interface Props {
+  className?: string;
+  container: HTMLElement;
+}
+
+export const QuizDialog: FC<Props> = ({ className, container }) => {
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            className={"fixed z-[999999] right-4 bottom-4"}
+            variant="default"
+          >
+            Quiz
+          </Button>
+        </DialogTrigger>
+        <DialogContent
+          className={cn("sm:max-w-[425px]", theme, className)}
+          portalContainer={container}
+        >
+          <VisuallyHidden>
+            <DialogHeader>
+              <DialogTitle>Quiz</DialogTitle>
+              <DialogDescription>Some quiz description</DialogDescription>
+            </DialogHeader>
+          </VisuallyHidden>
+          <div className={cn(theme, "dark:text-white")}>THE QUIZ CONTENT</div>
+          <DialogFooter>
+            <Button type="submit">Submit</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
